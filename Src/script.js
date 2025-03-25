80f15d8e6df52a668584481c5d87f221c5e1f532
Onduko <ondukodaisy@gmail.com>
2025-03-25 17:42:05 +0300

    Improved user interface

--- Src/script.js
+++ Src/script.js
@@ -4,15 +4,18 @@ let cityInput = document.querySelector("select");
 let cityName = document.querySelector(".cityName");
 let day = document.querySelector(".day");
 let time = document.querySelector(".time");
-localTime.innerHTML = `${moment().format("hh:mm A")}`;
+setInterval(function () {
+  localTime.innerHTML = `${moment().format("hh:mm:ss A")}`;
+  day.innerHTML = `${moment().format("ddd, D MMMM YYYY")}`;
+}, 1000);
 
-function getTime() {
-  let cityTime = moment().tz(`${cityInput.value}`).format(`hh:mm A`);
+setInterval(function getTime() {
+  let cityTime = moment().tz(`${cityInput.value}`).format(`hh:mm:ss A`);
   time.innerHTML = cityTime;
   cityName.innerHTML = cityInput.options[cityInput.selectedIndex].text; // Display the city name instead of the timezone
-  let date = moment().tz(`${cityInput.value}`).format("ddd D MMMM YYYY");
+  let date = moment().tz(`${cityInput.value}`).format("ddd, D MMMM YYYY");
   day.innerHTML = date;
-}
+}, 1000);
 
 function getCity(event) {
   event.preventDefault();
